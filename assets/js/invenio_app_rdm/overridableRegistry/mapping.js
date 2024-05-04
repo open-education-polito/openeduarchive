4090d244916d68f4f55d27e5bb528e15795d2976
libremente <surf@libremente.eu>
2024-05-04 14:23:25 +0200

    docs: adding comments in override section

--- assets/js/invenio_app_rdm/overridableRegistry/mapping.js
+++ assets/js/invenio_app_rdm/overridableRegistry/mapping.js
@@ -4,12 +4,10 @@
 // Invenio App RDM is free software; you can redistribute it and/or modify it
 // under the terms of the MIT License; see LICENSE file for more details.
 
-/**
- * Add here all the overridden components of your app.
- */
-
 import { MetadataToggle } from "../../components/deposit_ui/overrides/MetadataToggle";
 
+// Override components
+// Components set to null will be removed from UI
 export const overriddenComponents = {
     "ReactInvenioDeposit.FileUploaderToolbar.MetadataOnlyToggle.container": MetadataToggle,
     "InvenioAppRdm.Deposit.AccordionFieldFunding.container": () => null,
